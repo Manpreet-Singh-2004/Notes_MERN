5ea600ba973856d58156c3c72d6f3b38c3d353dd
Manpreet.Singh18 <manpreet.singh18@student.ufv.ca>
2025-06-06 14:57:39 -0700

    Added server and home backend

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+
+// Routes Paths
+const home = require('./Routes/home')
+
+
+const app = express()
+
+app.use(express.json())
+
+app.use((req, res, next) =>{
+    console.log(req.path, req.method)
+    next()
+})
+
+// Routes
+app.use('/', home)
+
+// Connecting to port
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        app.listen(process.env.PORT, ()=>{
+            console.log('Application connected to DB and running on port ', process.env.PORT);
+        })
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
